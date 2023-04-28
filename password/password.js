665a6d36c997bb2242dd4946a28e9d947c9a5637
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-04-28 19:01:33 +0530

    Updating UI of change password page

--- password/password.js
+++ password/password.js
@@ -3,7 +3,13 @@ async function submitInfo(e) {
         e.preventDefault();
         const emailId = e.target.email.value;
         let data = await axios.post('http://localhost:3000/password/forgotpassword', { emailId: emailId });
-
+        if (data.status === 201) {
+            document.getElementById('message').innerHTML = `<p>${data.data.message}</p>`;
+            let errorElement = document.getElementById('error');
+            errorElement.removeChild(document.getElementById('errorChild'));
+        } else {
+            throw new Error('User is not registered');
+        }
     } catch (error) {
         let errorElement = document.getElementById('error');
         if (error.response === undefined) {
